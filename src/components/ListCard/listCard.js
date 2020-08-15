7746a6a72f7bdf700bacd2c02b9fe669695520c9
TranHuuPhu1988 <tranhuuphu877@gmail.com>
2020-08-15 18:22:36 +0700

    asasasasas

--- src/components/ListCard/listCard.js
+++ src/components/ListCard/listCard.js
@@ -4,7 +4,7 @@ import { Col, Row } from 'antd';
 
 function listCard(props) {
   return (
-    <Row gutter={16}>
+    <Row gutter={[16, 16]}>
       <Col span={8}>
         <Cards/>
       </Col>
--- src/page/Homepage/homepage.js
+++ src/page/Homepage/homepage.js
@@ -2,16 +2,20 @@ import React from 'react';
 import MenuCom from "../../components/Menu/menu.js";
 import Header from '../../components/Header/header.js';
 import ListCard from "../../components/ListCard/listCard.js";
-import styles from "./Homepage.module.css";
-
+// import styles from "./Homepage.module.css";
+import { Row, Col } from 'antd';
 function homepage(props) {
     return (
         <div>
             <Header/>
-            <div className={styles.body}>
-                <MenuCom/>
-                <ListCard/>
-            </div>
+            <Row>
+                <Col span={4}>
+                    <MenuCom/>
+                </Col>
+                <Col span={19}>
+                    <ListCard/>
+                </Col>
+            </Row>
         </div>
     )
 }
